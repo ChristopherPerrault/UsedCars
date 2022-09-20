6de243ae6aac411167ad9ff21ea166bbfcd22427
ChristopherPerrault <christopherperrault@hotmail.com>
2022-09-20 17:01:18 -0400

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<!-- Universal footer -->
+
+<footer class="footer">
+    <div>© 2022 REGEX GROUP</div>
+</footer>
+<script src=""></script>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>UsedCars</title>
+
+    <authors made-by="Christoper Perrault, Matthew Reda, Seena Sabet-Kassouf" in="Sept 2022">
+
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="icon" href="../assets/img/favicon.png">
+
+        <meta name="keywords"
+            content="used sell cars car classified classifieds php mysql javascript crud auth authentiaction validation REST RESTful api apis phpmyadmin database db">
+
+        <meta name="description"
+            content="This web application takes the role as a mock used car dealership to demonstrate CRUD capabilities within a PHP, MySQL and Javascript environment. See attached supporting documents and/or readme for more information.">
+</head>
+
+<!-- Universal header -->
+
+<body>
+    <header>
+        <nav>
+            <div class="nav">
+                <a href="#">UsedCars</a>
+                <ul class="...">
+                    <li><a href="#" class="...">View Listings</a></li>
+                    <li><a href="#" class="...">Add Your Car</a></li>
+                    <li><a href="#" class="...">Manage Account</a></li>
+                    <li><a href="#" class="...">Log Out</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
