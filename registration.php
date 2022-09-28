b29ce59d65b2cd64ee43bc6e9bbc34a37a7b7f7a
ChristopherPerrault <christopherperrault@hotmail.com>
2022-09-28 10:02:49 -0400

    partial add of 'if username exists'

--- registration.php
+++ registration.php
@@ -1,10 +1,10 @@
 <!-- Header -->
-<?php 
+<?php
 include('templates/header-logged-out.php');
 ?>
 
 <?php
-$usernameErr = $fnameErr = $lnameErr = $phoneErr = $emailErr = $passwordErr = "";
+$usernameErr = $fnameErr = $lnameErr = $phoneErr = $emailErr = $passwordErr = $usernameExists = "";
 $username = $fname = $lname = $phone = $email = $password = "";
 
 $success = "";
@@ -20,86 +20,94 @@ function test_input($data)
 
 // ------------------ FORM VALIDATION ------------------
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if(empty($_POST['username'])) {
+    if (empty($_POST['username'])) {
         $usernameErr = "Username is required.";
-
-   } else {
-        $username = test_input($_POST['username']);
-        $usernameErr = "";
-
-        if(!preg_match("/^[a-zA-Z0-9]{5,20}$/", $username)) {
-            $usernameErr = "Must be a minimum of 5 characters with only numbers and letters.";
+    } else {
+        $usernameQuery = mysqli_query($con, "SELECT * FROM `users` WHERE username = '" . $_POST['username'] . "'");
+        if (mysqli_num_rows($usernameQuery)) {
+            $usernameExists = "This Username already exists.";
+            // $usernameExists = "This Username already exists.";
+        } else {
+            $usernameExists = "";
+            // $usernameErr = "";
         }
-   }
+    }
 
-   if(empty($_POST['fname'])) {
+
+    $username = test_input($_POST['username']);
+    $usernameErr = "";
+
+    if (!preg_match("/^[a-zA-Z0-9]{5,20}$/", $username)) {
+        $usernameErr = "Must be a minimum of 5 characters with only numbers and letters.";
+    }
+
+
+    if (empty($_POST['fname'])) {
         $fnameErr = "First Name is required.";
-
-   } else {
+    } else {
         $fnameErr = "";
         $fname = test_input($_POST['fname']);
 
-        if(!preg_match("/^[a-zA-Z]{3,50}$/", $fname)) {
+        if (!preg_match("/^[a-zA-Z]{3,50}$/", $fname)) {
             $fnameErr = "Only Captial letters and white spaces allowed.";
         }
-   }
+    }
 
-   if(empty($_POST['lname'])) {
+    if (empty($_POST['lname'])) {
         $lnameErr = "Last Name is required.";
-
-   } else {
+    } else {
         $lnameErr = "";
         $lname = test_input($_POST['lname']);
 
-        if(!preg_match("/^[a-zA-Z -]{3,50}$/", $lname)) {
+        if (!preg_match("/^[a-zA-Z -]{3,50}$/", $lname)) {
             $lnameErr = "Only Captial letters, white spaces and dashes allowed.";
         }
-   }
+    }
 
-   if(empty($_POST['phone'])) {
+    if (empty($_POST['phone'])) {
         $phoneErr = "Phone Number is Required.";
-
-   } else {
+    } else {
         $phoneErr = "";
         $phone = test_input($_POST['phone']);
-        
-        if(!preg_match("/^[0-9]{10,}$/", $phone)) {
+
+        if (!preg_match("/^[0-9]{10,}$/", $phone)) {
             $phoneErr = "Minimum of 10 digits required.";
         }
-   }
+    }
 
-   if(empty($_POST['email'])) {
+    if (empty($_POST['email'])) {
         $emailErr = "Email is required.";
-
-   } else {
+    } else {
         $emailErr = "";
         $email = test_input($_POST['email']);
 
-        if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
+        if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
             $emailErr = "Invalid email format.";
         }
-   } 
+    }
 
-   if(empty($_POST['password'])) {
+    if (empty($_POST['password'])) {
         $passwordErr = "Password is required.";
-
-   } else {
+    } else {
         $passwordErr = "";
         $password = test_input($_POST['password']);
 
-        if(!preg_match("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/", $password)) {
-            $passwordErr = "Password must be between 8 & 20 characters with at least one special character, one uppercase letter and one digit.";
+        if (!preg_match("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/", $password)) {
+            $passwordErr = "Password must be between 8 & 20 characters with at least one special character, one uppercase letter and
+one digit.";
         }
-   }
+    }
 
-   // ------------------ SENDING TO DATABASE ------------------
-   if(empty($usernameErr) &&
-      empty($fnameErr) && 
-      empty($lnameErr) && 
-      empty($phoneErr) &&
-      empty($emailErr) && 
-      empty($passwordErr)) {
-        
+    // ------------------ SENDING TO DATABASE ------------------
+    if (
+        empty($usernameErr) &&
+        empty($fnameErr) &&
+        empty($lnameErr) &&
+        empty($phoneErr) &&
+        empty($emailErr) &&
+        empty($passwordErr)
+    ) {
+
         $username = $_POST['username'];
         $fname = $_POST['fname'];
         $fname = ucwords(strtolower($fname), " \t\r\n'-");
@@ -108,26 +116,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $phone = $_POST['phone'];
         $email = $_POST['email'];
         $password = $_POST['password'];
-        
-        if($username == "admin") {
-            $usertype = "admin";
 
+        if ($username == "admin") {
+            $usertype = "admin";
         } else {
             $usertype = "user";
-
         }
 
-        $query = "INSERT into `users` (username, first_name, last_name, phone_number, email, `password`, usertype) VALUES ('" . $username . "', '" . $fname . "', '" . $lname . "', '" . $phone . "', '" . $email . "', '" . md5($password) . "', '" . $usertype . "')";
+        $query = "INSERT into `users` (username, first_name, last_name, phone_number, email, `password`, usertype) VALUES ('" .
+            $username . "', '" . $fname . "', '" . $lname . "', '" . $phone . "', '" . $email . "', '" . md5($password) . "', '" .
+            $usertype . "')";
 
-        #execute query 
+        #execute query
         $result = mysqli_query($con, $query);
         if ($result) {
             #if success
             $success = "You have registered successfully! <a href='login.php'> Log In here!</a>";
         }
     }
-
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -147,20 +156,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <!-- User Inputs -->
         <input type="text" name="username" placeholder="Username" /><br>
         <span class="error"><?php echo $usernameErr ?></span><br><br>
-        <input type="text" name="fname" placeholder="First Name"  /><br>
+        <span class="error"><?php echo $usernameExists ?></span><br><br>
+        <input type="text" name="fname" placeholder="First Name" /><br>
         <span class="error"><?php echo $fnameErr ?></span><br><br>
-        <input type="text" name="lname" placeholder="Last Name"  /><br>
+        <input type="text" name="lname" placeholder="Last Name" /><br>
         <span class="error"><?php echo $lnameErr ?></span><br><br>
-        <input type="text" name="phone" placeholder="Phone Number"  /><br>
+        <input type="text" name="phone" placeholder="Phone Number" /><br>
         <span class="error"><?php echo $phoneErr ?></span><br><br>
-        <input type="text" name="email" placeholder="Email"  /><br>
+        <input type="text" name="email" placeholder="Email" /><br>
         <span class="error"><?php echo $emailErr ?></span><br><br>
-        <input type="password" name="password" placeholder="Password"  /><br>
+        <input type="password" name="password" placeholder="Password" /><br>
         <span class="error"><?php echo $passwordErr ?></span><br><br>
 
         <input type="submit" name="submit" value="Register" />
     </form>
-    <p><?php echo $success ?></p> 
+    <p><?php echo $success ?></p>
 
 </div>
 
