8915379d9d31ddcc3f7634dd782568bd608f3380
Sansoon33 <104446906+Sansoon33@users.noreply.github.com>
2022-09-29 12:22:33 -0400

    Add username to nav + remove searchbar

--- templates/header-logged-in.php
+++ templates/header-logged-in.php
@@ -86,16 +86,11 @@
                         <li class="nav-item">
                             <a class="nav-link" href="logout.php">Log Out</a>
                         </li>
-
                     </ul>
 
-                    <!-- Search Bar -->
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-
                 </div>
+
+                <a class="nav-link"><b><?php echo $row['username'] ?></b></a>
             </div>
         </nav>
     </header>
