1c65eae621e38a481cb21646ea74614c040335c4
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2022-09-29 08:55:42 -0400

    Update header-logged-out.php

--- templates/header-logged-out.php
+++ templates/header-logged-out.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
 
-                <a class="navbar-brand" href="index.php">Used Cars</a>
+                <a class="navbar-brand" href="index-logged-out.php" title="View All Active Listings">Used Cars</a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
