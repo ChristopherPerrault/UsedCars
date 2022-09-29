28cc3ec846ceaa85da86625a4bda18a94b9033b2
ChristopherPerrault <christopherperrault@hotmail.com>
2022-09-29 18:52:31 -0400

    minor

--- templates/header-logged-out.php
+++ templates/header-logged-out.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <authors made-by="Christoper Perrault, Matthew Reda, Seena Sabet-Kassouf" in="Sept 2022">
 
-        <meta name="keywords" content="used sell cars car classified classifieds php mysql javascript crud auth authentiaction validation REST RESTful api apis phpmyadmin database db">
+        <meta name="keywords"
+            content="used sell cars car classified classifieds php mysql javascript crud auth authentiaction validation REST RESTful api apis phpmyadmin database db">
 
-        <meta name="description" content="This web application takes the role as a mock used car dealership to demonstrate CRUD capabilities within a PHP, MySQL and Javascript environment. See attached supporting documents and/or readme for more information.">
+        <meta name="description"
+            content="This web application takes the role as a mock used car dealership to demonstrate CRUD capabilities within a PHP, MySQL and Javascript environment. See attached supporting documents and/or readme for more information.">
 
         <!-- Link to CSS Bootstrap CDN -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
         <!-- Link to Custom CSS -->
         <link rel="stylesheet" href="css/style.css">
@@ -20,8 +23,9 @@
         <link rel="icon" href="assets/img/favicon.png">
 
         <!-- Link to JS Bootstrap CDN -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-        </script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+        </script> -->
 </head>
 
 <!-- Universal header -->
@@ -33,7 +37,10 @@
 
                 <a class="navbar-brand" href="index-logged-out.php" title="View All Active Listings">Used Cars</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation"><span
+                        class="navbar-toggler-icon"></span></button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
