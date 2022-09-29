23bd315a4f70995594410547de113dd77cc4cfa5
ChristopherPerrault <christopherperrault@hotmail.com>
2022-09-29 09:54:46 -0400

    minor edits

--- adminDashboard.php
+++ adminDashboard.php
@@ -7,18 +7,18 @@ include('templates/header-logged-in.php');
 
 
 <?php
-#check if user_id is set and store in variable
+// check if user_id is set and store in variable
 if (isset($_SESSION['user_id'])) {
 
     $user_id = $_SESSION['user_id'];
 
-    #query -> check if user exists
+    //  query -> check if user exists
     $query = "SELECT * FROM `users` WHERE user_id = '" . $user_id . "'";
 
-    #execute query 
+    //  execute query 
     $result = mysqli_query($con, $query) or die(mysqli_error($con));
 
-    #store user first name
+    //  store user first name
     while ($row = mysqli_fetch_assoc($result)) {
         $first_name = $row['first_name'];
     }
@@ -38,13 +38,13 @@ if (isset($_SESSION['user_id'])) {
 <!-- Body -->
 <div class="container mt-5">
     <!-- Echo User First Name -->
-    <h1 class="text-center"> Welcome <?php echo $first_name ?>!</h1>
+    <h1 class="text-center"> Welcome back, <?php echo $first_name ?>!</h1>
 
     <p class="text-center">
-        This is the administration home page -- You can chillout here
+        This is the administration dashboard
     </p>
 
-    <p class="text-center">You have special priviliges</p>
+    <p class="text-center">You have special privileges - Proceed with caution</p>
     <hr>
 </div>
 
